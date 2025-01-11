cef219fe4e1aa374a693b75206ae6412b0427003
noViceMin <admin@isamin.kr>
2025-01-11 15:02:19 +0900

    init

---
+++ src/main/kotlin/kr/isamin/manbangdoPlugin/ManbangdoPlugin.kt
@@ -0,0 +1,14 @@
+package kr.isamin.manbangdoPlugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ManbangdoPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
